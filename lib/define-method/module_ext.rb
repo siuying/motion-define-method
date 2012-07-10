dff0f3bf363bb9e5a8a353500690177f812c19e8
Francis Chong <francis@ignition.hk>
2012-07-10 18:44:07 +0800

    add a hack so that it work on metaclass

--- lib/define-method/module_ext.rb
+++ lib/define-method/module_ext.rb
@@ -1,11 +1,12 @@
+$define_method_blocks ||= {}
+
 class Module
   def define_method(name, *args, &blk)
-    @define_method_blocks ||= {}
-
-    if args.count > 0
-      @define_method_blocks[name] = args[0]
+    $define_method_blocks[self.to_s] ||= {}
+    if block_given?
+      $define_method_blocks[self.to_s][name] = lambda(&blk)
     elsif 
-      @define_method_blocks[name] = lambda(&blk)
+      $define_method_blocks[self.to_s][name] = args[0]      
     end
   end
 end
@@ -14,16 +15,20 @@ class Object
   attr_accessor :define_method_blocks
   alias_method :original_module_method_missing, :method_missing
   def method_missing(method, *args, &block)
-    self.class.define_method_blocks ||= {}
-    if self.class.define_method_blocks && self.class.define_method_blocks.include?(method)
-      return self.class.define_method_blocks[method].call(*args)
+    if $define_method_blocks[self.class.to_s] && $define_method_blocks[self.class.to_s].include?(method)
+      return $define_method_blocks[self.class.to_s][method].call(*args)
+
+    elsif self.respond_to?(:metaclass) && $define_method_blocks[self.metaclass.to_s] && $define_method_blocks[self.metaclass.to_s].include?(method)
+      return $define_method_blocks[self.metaclass.to_s][method].call(*args)
+
     else
       self.class.ancestors.each do |clazz|
-        if clazz.define_method_blocks && clazz.define_method_blocks.include?(method)
-          return clazz.define_method_blocks[method].call(*args)
+        if $define_method_blocks[clazz.to_s] && $define_method_blocks[clazz.to_s].include?(method)
+          return $define_method_blocks[clazz.to_s][method].call(*args)
         end
       end
       return original_module_method_missing(method, *args)
+
     end
   end
 end
